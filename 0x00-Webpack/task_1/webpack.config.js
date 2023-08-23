c5b9cfee3e8d2fa2b03e725b1cd0bd2d9a0c27fc
JoyEmma12 <ejoy0600@gmail.com>
2023-08-23 11:44:35 +0100

    bugs fixed

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "deploy"),
+    path: path.resolve(__dirname, "public"),
   },
   plugins: [
     new HtmlWebpackPlugin({
