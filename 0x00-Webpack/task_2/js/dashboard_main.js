b12a94a047ac35a93360c6fa6adb02132034b475
JoyEmma12 <ejoy0600@gmail.com>
2023-08-24 10:19:29 +0100

    fixing bugs

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,18 +1,31 @@
 import $ from "jquery";
-import debounce from "lodash/debounce";
+import css from "../css/main.css";
+import debounce from "lodash/debounce"; // Import the debounce function from Lodash
 
-$(document).ready(() => {
-  const countElement = $("#count");
-  const buttonElement = $("#clickButton");
+$(document).ready(function () {
+  // Add Paragraph Elements
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
 
+  // Add Button Element
+  $("body").append('<button id="click">Click here to get started</button>');
+
+  // Add Paragraph Element for Count
+  $("body").append('<p id="count"></p>');
+
+  // Add Another Paragraph
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  // Add updateCounter function
   let count = 0;
 
-  const updateCounter = () => {
-    count++;
-    countElement.text(`${count} clicks on the button`);
-  };
+  function updateCounter() {
+    count += 1;
+    $("#count").text(`${count} clicks on the button`);
+  }
 
-  const debouncedUpdateCounter = debounce(updateCounter, 300);
+  // Add click event handler using debounce
+  const debouncedUpdateCounter = _.debounce(updateCounter, 300); // Debounce for 300 milliseconds
 
-  buttonElement.on("click", debouncedUpdateCounter);
+  $("#click").on("click", debouncedUpdateCounter);
 });
