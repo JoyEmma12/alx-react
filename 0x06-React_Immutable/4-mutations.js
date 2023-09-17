a2f485ac6c9310a8d11ea801149c56254bd7ee09
JoyEmma12 <ejoy0600@gmail.com>
2023-09-17 03:21:45 +0100

    react immutable

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,16 @@
+import { Map } from "immutable";
+
+export default function maps1() {
+  const map = Map({
+    1: 'Liam',
+    2: 'Noah',
+    3: 'Elijah',
+    4: 'Oliver',
+    5: 'Jacob',
+    6: 'Lucas',
+  });
+};
+
+export default function maps2(object2) {
+    const map2 = map.withMutations(object2).set(2, 'Benjamin').set(4, 'Oliver');
+}
